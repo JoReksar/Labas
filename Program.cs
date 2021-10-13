121531b8e64466d1a977711190159202905f32a5
JoReksar <kokin.ru@gmail.com>
2021-10-13 09:47:35 +0300

    Laba1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Labas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            A a1 = new();
+            A a2 = new();
+            A a3 = new();
+            B b4 = new() { Obj1 = a2 };
+            C c5 = new() { Obj1 = a1, Obj2 = a3, Obj3 = b4 };
         }
+    }
+
+    class A { }
+
+    class B : A
+    {
+        public object Obj1 { get; init; }
+    }
+
+    class C : B
+    {
+        public object Obj2 { get; init; }
+        public object Obj3 { get; init; }
     }
 }
